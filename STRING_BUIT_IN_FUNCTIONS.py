6ea4909ae6e6976b3518d05c33ea480d238b929c
BISWAJEET BEHERA <69592952+biswajeetbehera85@users.noreply.github.com>
2022-09-13 10:11:55 +0530

    Create STRING_BUIT_IN_FUNCTIONS.py

---
+++ STRING_BUIT_IN_FUNCTIONS.py
@@ -0,0 +1,14 @@
+str='Hello_world'
+print(str.capitalize())
+print(str.center(32,'*'))
+print(str.count('he',0,len(str)))
+print(str.endswith('h',0,len(str)))
+print(str.find('llo',0,len(str)))
+print(str.upper())
+print(max('hello'))
+str2='765'
+print(str2.islower())
+str3='HeLlO WoRlD'
+print(str3.swapcase())
+str4='i am a pythonist'
+print(str4.title())
